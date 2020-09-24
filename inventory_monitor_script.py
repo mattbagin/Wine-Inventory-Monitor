d72f950d67704213dbd3afe1cd0cedfb13183db7
Matthew P. Bagin <matthew.bagin@gmail.com>
2020-09-24 10:10:18 -0400

    Corrected date comparisons. Added HTML email handling.

--- inventory_monitor_script.py
+++ inventory_monitor_script.py
@@ -3,6 +3,8 @@ import pandas as pd
 import smtplib
 import HashPassword as Hash
 from datetime import date
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 def get_inventory_sheet():
@@ -16,17 +18,27 @@ def get_inventory_sheet():
 
     complete_inv = pd.DataFrame(worksheet.get_all_records())
 
-    drink_soon = complete_inv.loc[(complete_inv["Consume By Date (Upper)"] >= today)]
+    complete_inv["Date Purchased"] = pd.to_datetime(
+        complete_inv["Date Purchased"]
+    ).dt.date
+    complete_inv["Consume By Date (Lower)"] = pd.to_datetime(
+        complete_inv["Consume By Date (Lower)"]
+    ).dt.date
+    complete_inv["Consume By Date (Upper)"] = pd.to_datetime(
+        complete_inv["Consume By Date (Upper)"]
+    ).dt.date
+
+    drink_soon = complete_inv.loc[(complete_inv["Consume By Date (Upper)"] <= today)]
 
     in_range = complete_inv.loc[
-        (complete_inv["Consume By Date (Lower)"] >= today)
-        & (complete_inv["Consume By Date (Upper)"] <= today)
+        (complete_inv["Consume By Date (Lower)"] <= today)
+        & (complete_inv["Consume By Date (Upper)"] >= today)
     ]
 
     return drink_soon, in_range
 
 
-def send_mail(user, pw, email_list):
+def send_mail(user, pw, email_list, msg):
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
@@ -34,15 +46,23 @@ def send_mail(user, pw, email_list):
 
     server.login(user, pw)
 
+    to = ""
     for e in email_list:
         to = to + e + ";"
 
-    subject = "Email Subject Line"
-    body = "Email body"
+    email_msg = MIMEMultipart("alternative")
 
-    msg = f"Subject: {subject}\n\n{body}"
+    email_msg["Subject"] = "Wine Inventory Notification - {}".format(
+        date.today().strftime("%Y/%m/%d")
+    )
 
-    server.sendmail(user, to, msg)
+    email_msg["From"] = user
+
+    msg_body = MIMEText(msg, "html")
+
+    email_msg.attach(msg_body)
+
+    server.sendmail(user, to, email_msg.as_string())
 
     print(f"Email sent to {to}")
 
@@ -55,3 +75,23 @@ if __name__ == "__main__":
 
     user = "vino.inventory.monitor@gmail.com"
     pw = Hash.decrypt(key, cipher_text).decode("utf-8")
+
+    drink_soon, in_range = get_inventory_sheet()
+
+    email_body = """
+        <html>
+            <body>
+                <p><h4 style="font-family:Roboto;font-size:10pt">The following wines are past their optimal aging time. Please consider drinking very soon!
+                <br>
+                {0}
+                <br>
+                <br>
+                The following wines are within their suggested age range based on their style.
+                {1}
+                </h4></p>    
+            </body>
+        </html>""".format(
+        drink_soon.to_html(), in_range.to_html()
+    )
+
+    send_mail(user=user, pw=pw, email_list=["matthew.bagin@gmail.com"], msg=email_body)
